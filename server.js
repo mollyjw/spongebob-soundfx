630593fed1970f773454990d2317507146d40990
MollyJW <mollyjwhitener@gmail.com>
2020-12-01 19:47:47 -0600

    updates to deploy to heroku

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(__dirname + '/dist/spongebob-soundfx'));
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname+'/dist/spongebob-soundfx/index.html'));});
+app.listen(process.env.PORT || 8080);
