97d09fe91f5f567ed1b38410e0200a6fbfce0cb0
MollyJW <mollyjwhitener@gmail.com>
2020-12-01 19:57:50 -0600

    changing port number to see if it fixes E Lifecycle error

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ const app = express();
 app.use(express.static(__dirname + '/dist/spongebob-soundfx'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+'/dist/spongebob-soundfx/index.html'));});
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 4200);
