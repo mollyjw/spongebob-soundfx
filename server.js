0349ca23af93a918244cf47641697b205daccb11
MollyJW <mollyjwhitener@gmail.com>
2020-12-06 14:03:15 -0600

    modified server.js file

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
+
 const app = express();
+
+const port = process.env.PORT || 4200;
+
 app.use(express.static(__dirname + '/dist/spongebob-soundfx'));
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname + '/dist/spongebob-soundfx/index.html'));});
-app.listen(process.env.PORT || 4200);
+
+app.get('/*', function(req,res) {res.sendFile(path.join(__dirname + '/dist/spongebob-soundfx/index.html'));});
+
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`App running on: http://localhost:${port}`));
+
+// app.listen(process.env.PORT || 4200);
