2777dd663b377cf49dd0e0842cc1136fc1380fe1
MollyJW <mollyjwhitener@gmail.com>
2020-12-06 13:33:30 -0600

    deleted and reinstalled npm

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/spongebob-soundfx'));
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+'/dist/spongebob-soundfx/index.html'));});
+  res.sendFile(path.join(__dirname + '/dist/spongebob-soundfx/index.html'));});
 app.listen(process.env.PORT || 4200);
